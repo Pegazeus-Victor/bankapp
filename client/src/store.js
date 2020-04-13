1acde5f067233485e6660fc8c41a8969f9d2b693
Pegazeus-Victor <61429368+Pegazeus-Victor@users.noreply.github.com>
2020-04-13 16:31:36 -0700

    inital commit

---
+++ client/src/store.js
@@ -0,0 +1,27 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from './reducers';
+
+const initialState = {};
+const middleware = [thunk];
+
+
+const store = process.env.NODE_ENV === 'production'
+    ?
+    createStore(
+        rootReducer,
+        initialState, //must put an initialstate if you have a combine reducer
+        applyMiddleware(...middleware)
+        )
+:
+  createStore(
+    rootReducer,
+    initialState, //must put an initialstate if you have a combine reducer
+         compose(
+            applyMiddleware(...middleware),
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+            )
+        );
+
+
+export default store;
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://Pegazeus:Sofi_9583@cluster0-qkghp.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+}
